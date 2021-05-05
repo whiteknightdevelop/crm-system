21fe08f7ae81b496d4ca16fdbe70e38db85a4be4
blackknightdevelop <blackknightdevelop@users.noreply.github.com>
2021-05-05 13:29:27 +0300

    initial commit

---
+++ ASP.NET Core Backend/Petadmin.Core/Exceptions/DeleteAllRemindersFailedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petadmin.Core.Exceptions
+{
+    public class DeleteAllRemindersFailedException : Exception
+    {
+        public DeleteAllRemindersFailedException()
+            : base("Not all reminders was deleted!") { }
+    }
+}
